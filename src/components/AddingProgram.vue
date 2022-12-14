01d5e74e14a2df3f6eb98cb95f9a188626c6d012
Булат Ибрагимов А <b.ibragimov@bars-open.ru>
2022-12-15 00:56:34 +0300

    Исправил баг с :key и вложенными v-for

--- src/components/AddingProgram.vue
+++ src/components/AddingProgram.vue
@@ -2,49 +2,74 @@
   <div class="container-fluid">
     <h3 class="mt-3">Конструктор программы</h3>
     <Form @submit="sendProgram" class="mb-4 mt-4" style="column-gap: 10%;">
-      <div style="width: 300px;" class="d-grid">
+      <div style="width: 310px;" class="d-grid">
         <Field type="text" class="form-control mb-1" placeholder="Название" name="name"/>
-        <Field as="textarea" class="form-control" rows="4" placeholder="Описание" style="resize: none;"
+        <Field as="textarea" class="form-control mb-1" rows="4" placeholder="Описание" style="resize: none;"
                name="description"/>
-        <FieldArray name="composition">
-          <div class="mt-2" v-for="(program, index) in programComposition" :key="index">
-            <div class="mb-1 d-flex justify-content-between align-items-center">
-              <span style="font-size: 18px;">{{ index + 1 }}</span>
-              <svg @click="removeExercise(index)" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                   fill="currentColor"
-                   class="bi bi-trash3-fill" viewBox="0 0 16 16">
+        <FieldArray name="training">
+          <div v-for="(training, trainingIndex) in programComposition" :key="trainingIndex"
+               class="d-grid mt-2 pb-2 px-2 rounded-3 bg-gradient" style="background: #e8e8e8;">
+            <Field as="input" :name="`training[${trainingIndex}].number`" :value="trainingIndex+1" disabled
+                   class="d-none"></Field>
+            <div class="h5 text-center ">
+              <svg @click="removeTraining(training.id)" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                   fill="red" class="bi bi-x-lg"
+                   viewBox="0 0 16 16">
                 <path
-                    d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                    d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
               </svg>
+              Тренировка №{{ trainingIndex + 1 }}
             </div>
-            <Field :name="`composition[${index}].exercise`" as="select" class="form-select mb-1" aria-label="Default select example"
-                   :value="program.exerciseId"
-                   @change="this.programComposition[index].exerciseId=$event.target.value" :key="index">
-              <!--          <select class="form-select mb-1" aria-label="Default select example" @change="print">-->
-              <option selected hidden disabled>Выберите упражнение</option>
-              <optgroup v-for="(muscle, musIndex) in allExercises.keys()" :key="musIndex" :label="muscle">
-                <option v-for="ex in allExercises.get(muscle).keys()" :key="ex" :value="ex">
-                  {{ allExercises.get(muscle).get(ex) }}
-                </option>
-              </optgroup>
-            </Field>
-            <div class="mb-1 d-flex gap-1">
-              <div class="form-floating">
-                <Field type="number" class="form-control" :value="program.sets" :name="`composition[${index}].amountsOfsSets`"
-                       @input="this.programComposition[index].sets=$event.target.value"
-                       placeholder="Подходы"/>
-                <label>Подходы</label>
+            <FieldArray name="composition">
+              <div class="mt-2" v-for="(program, index) in programComposition.find(x => x.id === training.id).training "
+                   :key="program.id">
+                <div class="mb-1 d-flex justify-content-between align-items-center">
+                  <span style="font-size: 18px;">{{ index + 1 }}</span>
+                  <svg @click="removeExercise(training.id, program.id)" xmlns="http://www.w3.org/2000/svg" width="16"
+                       height="16"
+                       fill="red"
+                       class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                  </svg>
+                </div>
+                <Field :name="`training[${trainingIndex}].composition[${index}].exercise`" as="select"
+                       class="form-select mb-1"
+                       aria-label="Default select example"
+                       :value="program.exerciseId"
+                       @change="this.programComposition[trainingIndex].training[index].exerciseId=$event.target.value"
+                       :key="index">
+                  <option selected hidden disabled>Выберите упражнение</option>
+                  <optgroup v-for="(muscle) in allExercises.keys()" :key="muscle" :label="muscle">
+                    <option v-for="ex in allExercises.get(muscle).keys()" :key="ex" :value="ex">
+                      {{ allExercises.get(muscle).get(ex) }}
+                    </option>
+                  </optgroup>
+                </Field>
+                <div class="mb-1 d-flex gap-1">
+                  <div class="form-floating">
+                    <Field type="number" class="form-control" :value="program.sets"
+                           :key="index"
+                           :name="`training[${trainingIndex}].composition[${index}].amountsOfsSets`"
+                           @input="this.programComposition[trainingIndex].training[index].sets=$event.target.value"
+                           placeholder="Подходы"/>
+                    <label>Подходы</label>
+                  </div>
+                  <div class="form-floating">
+                    <Field type="number" class="form-control" placeholder="Повторения"
+                           :key="index"
+                           :name="`training[${trainingIndex}].composition[${index}].amountsOfRepetitions`"
+                           @input="this.programComposition[trainingIndex].training[index].repetitions=$event.target.value"
+                           :value="program.repetitions"/>
+                    <label>Повторения</label>
+                  </div>
+                </div>
               </div>
-              <div class="form-floating">
-                <Field type="number" class="form-control" placeholder="Повторения" :name="`composition[${index}].amountsOfRepetitions`"
-                       @input="this.programComposition[index].repetitions=$event.target.value"
-                       :value="program.repetitions"/>
-                <label>Повторения</label>
-              </div>
-            </div>
+            </FieldArray>
+            <button type="button" class="btn btn-dark" @click="addExercise(trainingIndex)">Добавить упражнение</button>
           </div>
-      </FieldArray>
-        <button type="button" class="btn btn-dark" @click="addExercise">Добавить упражнение</button>
+        </FieldArray>
+        <button type="button" class="btn btn-danger  mt-2" @click="addTraining()">Добавить тренировку</button>
         <input class="mt-4 btn btn-lg btn-primary d-block" type="submit" value="Сохранить программу"/>
       </div>
     </Form>
@@ -68,28 +93,62 @@ export default {
       name: null,
       description: null,
       programComposition: [{
-        exerciseText: null,
-        exerciseId: "Выберите упражнение",
-        sets: null,
-        repetitions: null
+        id: Date.now(),
+        training: [{
+          id: Date.now(),
+          exerciseText: null,
+          exerciseId: "Выберите упражнение",
+          sets: null,
+          repetitions: null
+        }]
       }],
       allExercises: new Map()
     };
   },
   methods: {
-    addExercise() {
-      this.programComposition.push({
+    addExercise(trainingIndex) {
+      this.programComposition[trainingIndex].training.push({
+        id: Date.now(),
         exerciseText: null,
         exerciseId: "Выберите упражнение",
         sets: null,
         repetitions: null
       })
     },
-    removeExercise(index) {
-      this.programComposition.splice(index, 1)
+    addTraining() {
+      this.programComposition.push({
+        id: Date.now(),
+        training: [{
+          id: Date.now(),
+          exerciseText: null,
+          exerciseId: "Выберите упражнение",
+          sets: null,
+          repetitions: null
+        }]
+      })
+    },
+    removeExercise(trainingId, programId) {
+      console.log(trainingId, programId)
+      // this.programComposition[trainingId].training.splice(programId, 1)
+      this.programComposition.forEach(
+          x => {
+            if (x.id === trainingId) {
+              x.training = this.programComposition.find(x => x.id === trainingId).training.filter(x => x.id !== programId)
+            }
+          }
+      )
+    },
+    removeTraining(trainingId) {
+      console.log(trainingId)
+      // this.programComposition.splice(trainingId, 1)
+      this.programComposition = this.programComposition.filter(x => x.id !== trainingId)
     },
     sendProgram(program) {
-      programService.postProgram(program)
+      // programService.postProgram(program)
+      console.log(program)
+      setTimeout(function () {
+        debugger;
+      }, 0)
     }
   },
   mounted() {
